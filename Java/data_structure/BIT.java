83a79499980bf46b9553ebff559911321715cccf
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2024-11-05 10:58:40 +0900

    半角空白を入れて見やすくした

--- Java/data_structure/BIT.java
+++ Java/data_structure/BIT.java
@@ -2,12 +2,13 @@
 class BIT {
     int size;
     long[] bit;
+
     BIT (int size) {
         this.size = size;
         this.bit = new long[size + 1];
     }
 
-    // O(logN): インデックス idx の値に val を加算するメソッド
+    // O(log N): インデックス idx の値に val を加算するメソッド
     public void add(int idx, long val) {
         while (idx <= this.size) {
             this.bit[idx] += val;
@@ -15,7 +16,7 @@ class BIT {
         }
     }
 
-    // O(logN): 区間 1 ~ idx の和を求める計算用のメソッド
+    // O(log N): 区間 1 ~ idx の和を求める計算用のメソッド
     private long sum(int idx) {
         long result = 0L;
         while (idx > 0) {
@@ -25,12 +26,12 @@ class BIT {
         return result;
     }
 
-    // O(logN): 区間 1 ~ idx の和を求めるメソッド
+    // O(log N): 区間 1 ~ idx の和を求めるメソッド
     public long getSum(int idx) {
         return sum(idx);
     }
 
-    // O(logN): 区間 idx1 ~ idx2 の和を求めるメソッド
+    // O(log N): 区間 idx1 ~ idx2 の和を求めるメソッド
     public long getSum(int idx1, int idx2) {
         return sum(idx2) - sum(idx1 - 1);
     }
--- Python/data_structure/BIT.py
+++ Python/data_structure/BIT.py
@@ -4,13 +4,13 @@ class BIT:
         self.size = size
         self.bit = [0] * (size + 1)
 
-    # O(logN): インデックス idx の値に val を加算するメソッド
+    # O(log N): インデックス idx の値に val を加算するメソッド
     def add(self, idx, val):
         while idx <= self.size:
             self.bit[idx] += val
             idx += idx & -idx
 
-    # O(logN): 区間 1 ~ idx の和を求める計算用のメソッド
+    # O(log N): 区間 1 ~ idx の和を求める計算用のメソッド
     def _sum(self, idx):
         result = 0
         while idx > 0:
@@ -18,7 +18,7 @@ class BIT:
             idx -= idx & -idx
         return result
 
-    # O(logN): 区間 1 ~ idx1 または 区間 idx1 ~ idx2 の和を求めるメソッド
+    # O(log N): 区間 1 ~ idx1 または 区間 idx1 ~ idx2 の和を求めるメソッド
     def get_sum(self, idx1, idx2 = None):
         if idx2 is None:
             return self._sum(idx1)
