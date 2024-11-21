ae976c3614afc1183eb9c3bd47ec56b4a31f29f7
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2024-11-21 13:19:43 +0900

    java の BIT に対するテストコードの追加

--- data_structure/BIT/java/BIT.java
+++ data_structure/BIT/java/BIT.java
@@ -1,7 +1,7 @@
 // フェニック木 (binary indexed tree: BIT) クラス
 class BIT {
     private int size;
-    private long[] bit;
+    protected long[] bit;
 
     BIT (int size) {
         this.size = size;
---
+++ data_structure/BIT/java/BITTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class BITTest {
+    @Test
+    public void testAdd() {
+        final int SIZE = 4;
+        BIT bit = new BIT(SIZE);
+        bit.add(1, 3);
+        bit.add(2, 2);
+        bit.add(3, 5);
+        bit.add(4, 1);
+
+        assertFalse(Arrays.equals(bit.bit, new long[]{0, 3, 2, 5, 1}));
+        assertArrayEquals(bit.bit, new long[]{0, 3, 5, 5, 11});
+    }
+
+    @Test
+    public void testGetSum() {
+        final int SIZE = 5;
+        BIT bit = new BIT(SIZE);
+        bit.add(1, 3);
+        bit.add(2, 2);
+        bit.add(3, 5);
+        bit.add(4, 1);
+        bit.add(5, 5);
+
+        assertEquals(bit.getSum(1), 3);
+        assertEquals(bit.getSum(2), 5);
+        assertEquals(bit.getSum(3), 10);
+        assertEquals(bit.getSum(4), 11);
+        assertEquals(bit.getSum(5), 16);
+
+        assertEquals(bit.getSum(1, 5), 16);
+        assertEquals(bit.getSum(2, 4), 8);
+        assertEquals(bit.getSum(3, 3), 5);
+    }
+}
