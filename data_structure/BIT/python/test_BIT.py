9406a3e7e7ff69b19d45f52caafb016e8ba418eb
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2024-11-21 13:11:16 +0900

    python の BIT に対するテストコードの追加

---
+++ data_structure/BIT/python/test_BIT.py
@@ -0,0 +1,37 @@
+import unittest
+from BIT import *
+
+class TestBIT(unittest.TestCase):
+    def test_add(self):
+        """値の追加のテスト"""
+        SIZE = 4
+        bit = BIT(SIZE)
+        bit.add(1, 3)
+        bit.add(2, 2)
+        bit.add(3, 5)
+        bit.add(4, 1)
+        self.assertNotEqual(bit.bit, [0, 3, 2, 5, 1])
+        self.assertEqual(bit.bit, [0, 3, 5, 5, 11])
+
+    def test_get_sum(self):
+        """区間和のテスト"""
+        SIZE = 5
+        bit = BIT(SIZE)
+        bit.add(1, 3)
+        bit.add(2, 2)
+        bit.add(3, 5)
+        bit.add(4, 1)
+        bit.add(5, 5)
+
+        self.assertEqual(bit.get_sum(1), 3)
+        self.assertEqual(bit.get_sum(2), 5)
+        self.assertEqual(bit.get_sum(3), 10)
+        self.assertEqual(bit.get_sum(4), 11)
+        self.assertEqual(bit.get_sum(5), 16)
+
+        self.assertEqual(bit.get_sum(1, 5), 16)
+        self.assertEqual(bit.get_sum(2, 4), 8)
+        self.assertEqual(bit.get_sum(3, 3), 5)
+
+if __name__ == "__main__":
+    unittest.main()
