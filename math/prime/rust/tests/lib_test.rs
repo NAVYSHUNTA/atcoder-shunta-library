37fb5a3e6b5f3d43fbf2dc4faa0db76eaeb7b08e
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2024-12-31 05:05:04 +0900

    可読性向上

--- math/prime/rust/tests/lib_test.rs
+++ math/prime/rust/tests/lib_test.rs
@@ -3,34 +3,34 @@ use rust::*;
 #[test]
 fn test_is_prime() {
     // 素数に対する素数判定テスト
-    assert_eq!(true, is_prime(2i8));
-    assert_eq!(true, is_prime(3i32));
-    assert_eq!(true, is_prime(5i64));
-    assert_eq!(true, is_prime(7i128));
+    assert_eq!(true, is_prime(2_i8));
+    assert_eq!(true, is_prime(3_i32));
+    assert_eq!(true, is_prime(5_i64));
+    assert_eq!(true, is_prime(7_i128));
 }
 
 #[test]
 fn test_is_not_prime() {
     // 合成数に対する素数判定テスト
-    assert_eq!(false, is_prime(0i8));
-    assert_eq!(false, is_prime(1i32));
-    assert_eq!(false, is_prime(4i64));
-    assert_eq!(false, is_prime(-1i128));
-    assert_eq!(false, is_prime(-6i128));
+    assert_eq!(false, is_prime(0_i8));
+    assert_eq!(false, is_prime(1_i32));
+    assert_eq!(false, is_prime(4_i64));
+    assert_eq!(false, is_prime(-1_i128));
+    assert_eq!(false, is_prime(-6_i128));
 }
 
 #[test]
 fn test_is_prime_large() {
     // 大きな素数に対する素数判定テスト
-    assert_eq!(true, is_prime(200560490131i64));
-    assert_eq!(true, is_prime(92709568269121i128));
-    assert_eq!(true, is_prime(99999999999973i128));
+    assert_eq!(true, is_prime(200560490131_i64));
+    assert_eq!(true, is_prime(92709568269121_i128));
+    assert_eq!(true, is_prime(99999999999973_i128));
 }
 
 #[test]
 fn test_is_not_prime_large() {
     // 大きな合成数に対する素数判定テスト
-    assert_eq!(false, is_prime(200560490113i64));
-    assert_eq!(false, is_prime(2005604910109i128));
-    assert_eq!(false, is_prime(92709568269119i128));
+    assert_eq!(false, is_prime(200560490113_i64));
+    assert_eq!(false, is_prime(2005604910109_i128));
+    assert_eq!(false, is_prime(92709568269119_i128));
 }
